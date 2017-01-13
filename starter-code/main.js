696bc5758a196e5570ec230fb039e543a021dbf0
Wes <wes.coffay@gmail.com>
2017-01-13 15:33:20 -0500

    added javascript cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,18 @@ var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
-
-if (cardOne === cardFour) {
+/*if (cardOne === cardFour) {
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
+}*/
+
+function createCards () {
+var gameBoard = document.getElementById('game-board');
+for (i=0; i<4; i++) {
+	var flipCard = document.createElement('div')
+	flipCard.className = 'card';
+	document.getElementById('game-board').appendChild(flipCard);
+}	
 }
+createCards();
